b990ef0837ce07ccb2ebbb777409c6869e894559
MengChiaLee <77147836+MengChiaLee@users.noreply.github.com>
2021-03-24 16:50:38 +0800

    Add files via upload

---
+++ quiz1.js
@@ -0,0 +1,7 @@
+function change()
+{var header = document.getElementById("hid123");
+header.innerHTML = "CSIE@CGU";
+var para = document.getElementById("pid123");
+para.innerHTML = "?c?o¡Lo?o¡¦I!I!I.";}
+function myFunction() { var btn = document.createElement("BUTTON"); btn.innerHTML = "CLICK ME"; document.body.appendChild(btn).addEventListener("click", change); }
+
